7b1878de31ca5149afda1b4303a3d6ff6cf6d0b7
Geek_红影 <liuhongfeng239@163.com>
2019-12-09 23:33:20 +0800

    update(algorithm) move zeroes

--- algorithm/src/main/java/com/geek/algorithm/practice/MoveZeroes.java
+++ algorithm/src/main/java/com/geek/algorithm/practice/MoveZeroes.java
@@ -9,7 +9,7 @@ public class MoveZeroes {
 
     public static void main(String[] args) {
         int[] nums = {0, 0, 1};
-        moveZeroes3(nums);
+        moveZeroes(nums);
     }
 
     /**
@@ -17,7 +17,8 @@ public class MoveZeroes {
      * 示例:
      * 输入: [0,1,0,3,12]
      * 输出: [1,3,12,0,0]
-     * 不通过
+     * <p>
+     * 对于 {0, 0, 1} 情况，不通过
      *
      * @param nums
      */
@@ -40,6 +41,7 @@ public class MoveZeroes {
      * [0,1,0,3,12]
      * [1,3,12,0,0]
      * 通过
+     * 时间复杂度 O(n^2)
      *
      * @param nums
      */
@@ -64,6 +66,7 @@ public class MoveZeroes {
      * 国际站解法
      * [0,1,0,3,12]
      * [0,0,1]
+     * 时间复杂度 O(n)
      *
      * @param nums
      */
@@ -79,6 +82,54 @@ public class MoveZeroes {
         }
         for (int num : nums) {
             System.out.println(num);
+        }
+    }
+
+    /**
+     * 覃超
+     * 算法训练营视频中解法
+     * 时间复杂度 O(n)
+     *
+     * @param nums
+     */
+    public static void moveZeroes4(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != 0) {
+                if (i != j) {
+                    // 优化处理
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+
+    /**
+     * 国际站
+     * 滚雪球解法
+     * [0,1,0,3,12]
+     * [1,0,0,3,12]
+     * <p>
+     * 时间复杂度 O(n)
+     * snowBallSize 雪球大小
+     *
+     * @param nums
+     */
+    public static void moveZeroes5(int[] nums) {
+        int snowBallSize = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                snowBallSize++;
+            } else if (snowBallSize > 0) {
+                int t = nums[i];
+                nums[i] = 0;
+                nums[i - snowBallSize] = t;
+            }
         }
     }
 
